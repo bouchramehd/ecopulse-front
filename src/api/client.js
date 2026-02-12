e047d6f797a2f3301eed5b57449b5a498d53d525
bouchra mehdaoui <bouchra-mehdaoui@hotmail.com>
2026-02-13 00:34:17 +0100

    Connect frontend to Render backend

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") ||
+  "http://127.0.0.1:9000";
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:9000",
+  baseURL,
   timeout: 15000,
 });
